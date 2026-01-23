da70a60c073df4234cd95368153eadb6ec02ad65
Nat2006-cb <swetha.usharani6@gmail.com>
2026-01-23 03:27:58 +0000

    -

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -1,8 +1,9 @@
 package com.examly.springapp.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
+import jakarta.persistence.Id;
 
 
 @Entity
@@ -16,11 +17,29 @@ public class User {
     public User(){
 
     }
+    public long getUserId() {
+        return userId;
+    }
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
-
+    
     
     
 
