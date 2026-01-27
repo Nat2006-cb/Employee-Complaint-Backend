e9c14617713191e4d9f87f06821cae7156f6c997
Nat2006-cb <swetha.usharani6@gmail.com>
2026-01-27 02:27:12 +0000

    service-controller-repository are created

--- springapp/src/main/java/com/examly/springapp/controller/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controller/UserController.java
@@ -1,8 +1,76 @@
 package com.examly.springapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.examly.springapp.model.User;
+import com.examly.springapp.service.UserServiceImpl;
+
 @RestController
+@RequestMapping("/api")
 public class UserController {
-    
+    @Autowired 
+    UserServiceImpl userServiceImpl;
+
+    @PostMapping("/users")
+    public ResponseEntity<User> create(@RequestBody User user){
+        User res=userServiceImpl.create(user);
+        if(res==null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity<>(res,HttpStatus.CREATED);
+        }
+    }
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getData(){
+        List<User> res=userServiceImpl.getData();
+        if(res.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else{
+            return new ResponseEntity<>(res,HttpStatus.OK);
+        }
+    }
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getid(@PathVariable Long id){
+        User res=userServiceImpl.getid(id);
+        if(res==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else{
+            return new ResponseEntity<>(res,HttpStatus.OK);
+        }
+    }
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> update(@RequestBody User user,@PathVariable Long id){
+        User res=userServiceImpl.update(user,id);
+        if(res==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else{
+            return new ResponseEntity<>(res,HttpStatus.OK);
+        }
+    }
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable Long id){
+        Boolean res=userServiceImpl.delete(id);
+        if(res){
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
 }
--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -1,8 +1,56 @@
 package com.examly.springapp.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.User;
+import com.examly.springapp.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService{
-    
+    @Autowired
+    UserRepository userRepository;
+
+    public User create(User user){
+        try{
+            return userRepository.save(user);
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+    public List<User> getData(){
+        return userRepository.findAll();
+    }
+    public User getid(Long id){
+        Optional<User> res=userRepository.findById(id);
+        if(res.isPresent()){
+            return res.get();
+        }
+        else{
+            return null;
+        }
+    }
+    public User update(User user,Long id){
+        if(userRepository.existsById(id)){
+            user.setUserId(id);
+            return userRepository.save(user);
+        }
+        else{
+            return null;
+        }
+    }
+    public Boolean delete(Long id){
+        Optional<User> res=userRepository.findById(id);
+        if(res.isPresent()){
+            userRepository.delete(res.get());
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
